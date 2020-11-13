7851e8f39bd07f804e92db08ba15f858e0bca8d7
Shakti Prasad <shprasad@ansys.com>
2020-11-13 20:17:36 +0530

    Added Builder and Concrete Builder classes

--- Builder/PizzaBuilder.cpp
+++ Builder/PizzaBuilder.cpp
@@ -3,6 +3,7 @@
 #include<string>
 using namespace std;
 
+/* Product class*/
 class Pizza
 {
 public:
@@ -29,6 +30,63 @@ private:
     string m_topping;
 };
 
+/* Abstract Builder class */
+class PizzaBuilder
+{
+public:
+    /* virtual destructor should be there if virtual member functions exits */
+    virtual ~PizzaBuilder() {};
+    virtual void buildDough() = 0;
+    virtual void buildSauce() = 0;
+    virtual void buildTopping() = 0;
+    Pizza* getPizza()
+    {
+        return m_pizza.get();
+    }
+    void createNewPizzaProduct()
+    {
+        m_pizza = make_unique<Pizza>();
+    }
+
+protected:
+    unique_ptr<Pizza> m_pizza;
+};
+
+/* Concrete Builder 1 */
+class HawaiianPizzaBuilder: public PizzaBuilder
+{
+    virtual ~HawaiianPizzaBuilder() {};
+    virtual void buildDough()
+    {
+        m_pizza->setDough("cross");
+    }
+    virtual void buildSauce()
+    {
+        m_pizza->setSauce("mild");
+    }
+    virtual void buildTopping()
+    {
+        m_pizza->setTopping("ham+pineapple");
+    }
+};
+
+/* Concrete Builder 2 */
+class SpicyPizzaBuilder : public PizzaBuilder
+{
+    virtual ~SpicyPizzaBuilder() {};
+    virtual void buildDough()
+    {
+        m_pizza->setDough("pan baked");
+    }
+    virtual void buildSauce()
+    {
+        m_pizza->setSauce("hot");
+    }
+    virtual void buildTopping()
+    {
+        m_pizza->setTopping("pepperoni+salami");
+    }
+};
 
 int main()
 {
